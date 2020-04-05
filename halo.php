2a36723304f059a57ff742ba15b45adbe88752bd
Farid Anwar <52661004+novalers123@users.noreply.github.com>
2020-04-05 19:22:32 +0700

    Update halo.php

--- halo.php
+++ halo.php
@@ -1,4 +1,7 @@
 <?php
+//php tidak memerlukan indentasi layaknya python
+//php tidak harus menentukan jenis variabel yang akan diisi
+//php versi 7 keatas susah dijebol cok
 
 if (1==1){
 	echo "hello world";
