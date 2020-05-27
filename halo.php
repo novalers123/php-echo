fc76c8bcfd0fc39acb4be41e4e6ed3657bff93a0
Farid Anwar <52661004+novalers123@users.noreply.github.com>
2020-05-27 13:37:51 +0700

    Umpdate just Update

--- halo.php
+++ halo.php
@@ -1,11 +1,8 @@
 <?php
-//php tidak memerlukan indentasi layaknya python
-//php tidak harus menentukan jenis variabel yang akan diisi
-//iki komentar cok
+// perulangan sebanyak unlimited-kali
 
-if (1==1){
-	echo "hello world";
-} else {}
-
+while (1 == intval('1e10')) {
+	echo "Aebbb Gess" . "\n";
+}
+ 
 ?>
-Wah parah sih bro, tapi gue sih owh aja..
